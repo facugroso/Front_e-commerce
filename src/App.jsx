d966ad07875bea5965f72812d8e577016cfec2f7
guzmangj <guzmangj96@gmail.com>
2023-06-14 12:31:56 -0300

    Bootstrap en index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import "./Header.css";
+
 function Header() {
   return (
     <>
       <header>
         <div>
           <img
+            className="img-fluid"
             src="https://hardzone.es/app/uploads-hardzone.es/2023/05/nuevos-auriculares-hyperx-cloud-iii.jpg"
             alt="Image header"
           />
