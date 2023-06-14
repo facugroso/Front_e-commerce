3ba8371025c3ff7604d70e0bd4f19c44a69f46c2
facugroso <110434971+facugroso@users.noreply.github.com>
2023-06-14 17:58:01 -0300

    carpetas incompletas

---
+++ src/components/CartOffCanvas.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-bootstrap/lib/Navbar";
+
+function cart({ cart, ...props }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow} className="me-2">
+        👜
+      </Button>
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>My Bag</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Your cart is currently empty.
+          <div>
+            <div className="row no-margin bg-grey">
+              <div>
+                <img src="" alt="imagen del producto" />{" "}
+              </div>
+              <div>
+                <Link to="">HyperX SoloCast - USB Microphone</Link>
+                <span>Color: Black</span>
+                <p>$ price</p>
+                <input> (-) contador (+)</input>
+                <button> remove </button>
+              </div>
+            </div>
+
+            <div>
+              <p>Subtotal:price</p>
+              <button>VIEW BAG</button>
+            </div>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default cart;
---
+++ src/components/DeleteProduct.jsx
@@ -0,0 +1,31 @@
+// // import axios from "axios";
+// // import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
+// // import { deleteTweet } from "../redux/tweetSlice";
+
+// function DeleteProduct({product}) {
+//     const dispatch = useDispatch();
+//     // const token = useSelector( state => state.user.token )
+//     // const handleSubmit = async (e) => {
+//     //     e.preventDefault();
+//     //     const response = await axios({
+//     //         method: "DELETE",
+//     //         url: `http://localhost:3000/tweets/${tweet._id}`,
+//     //         headers:{
+//     //             Authorization: `Bearer ${token}`
+//     //         }
+//     //     })
+//       dispatch(deleteProduct(product._id));
+//     // }
+//     return (
+//         <div>
+//             <form method="POST" onSubmit={handleSubmit}>
+//             <button className="delete">
+//                 <i className="redColor bi bi-trash-fill"></i>
+//             </button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default DeleteProduct;
