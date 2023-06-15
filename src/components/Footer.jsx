3a284293a93b3f1409e4f3bdceb3a734f05851b1
facugroso <110434971+facugroso@users.noreply.github.com>
2023-06-15 18:53:54 -0300

    retoque de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,45 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Suport</h1>
+            <p>Product Suport</p>
+            <p>Contact Suport</p>
+            <p>Shipping Information</p>
+            <p>Refund Policy</p>
+            <p>HP/HYPERX Visual Content Terms of Use</p>
+          </div>
+          <div className="col">
+            <h1>Company</h1>
+            <p>About HyperX</p>
+            <p>About HP</p>
+            <p>Careers</p>
+            <p>Press</p>
+            <p>Privacy</p>
+            <p>Terms of Use</p>
+            <p>Limited Warranty Statement</p>
+            <p>Accessibility</p>
+            <p>Use of Cookies</p>
+            <p>Personal Data Rights</p>
+            <p>Terms of Service</p>
+            <p>Refund policy</p>
+          </div>
+          <div className="col">
+            <h1>Other Resource</h1>
+            <p>NGENUITY Software</p>
+            <p>Blog</p>
+            <p>Partner Program</p>
+            <p>Student Discount</p>
+          </div>
+          <div className="col">
+            <img src="" alt="IMAGEN HIPERX" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
