5ce0a42f2576a2e47a0f98e48a72070cb4bb06d4
valentinalr <valentina.lerose@outlook.com>
2023-06-15 02:07:56 -0300

    navbar2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
