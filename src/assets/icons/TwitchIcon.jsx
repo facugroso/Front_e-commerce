369f7fc6602d0b9feb8207bf514c48146a66652f
guzmangj <guzmangj96@gmail.com>
2023-06-16 11:32:04 -0300

    Actualización footer

---
+++ src/assets/icons/TwitchIcon.jsx
@@ -0,0 +1,20 @@
+function TwitchIcon() {
+  return (
+    <svg
+      class="icon icon__twitch"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="white"
+        d="M19.3163 13.137L16.3898 16.137H11.7996L9.29399 18.7055V16.137H5.52561V3.70548H19.3163V13.137ZM3.84187 2L3 5.43151V20.863H6.76837V23H8.853L10.9376 20.863H14.3051L21 14V2H3.84187Z"
+      ></path>
+      <path
+        fill="white"
+        d="M10 13H11.5911V8H10V13ZM14.4089 13H16V8H14.4089V13Z"
+      ></path>
+    </svg>
+  );
+}
+
+export default TwitchIcon;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,128 @@
-import Logo from "../assets/icons/Logo";
+import Logo from "../assets/icons/logofooter.svg";
+import InstagramIcon from "../assets/icons/instagramicon";
+import FacebookIcon from "../assets/icons/FacebookIcon";
+import TwitterIcon from "../assets/icons/TwitterIcon";
+import DiscordIcon from "../assets/icons/DiscordIcon";
+import TwitchIcon from "../assets/icons/TwitchIcon";
+import TikTokIcon from "../assets/icons/TikTokIcon";
+import YouTubeIcon from "../assets/icons/YouTubeIcon";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 function Footer() {
   return (
     <>
-      <div className="conteiner">
-        <div className="row">
-          <div className="col">
-            <div className="row">
-              <div className="col">
-                <h5 className="fs-5 fw-bold">Suport</h5>
-                <section className="options">
-                  <p>Product Suport</p>
-                  <p>Contact Suport</p>
-                  <p>Shipping Information</p>
-                  <p>Refund Policy</p>
-                  <p>HP/HYPERX Visual Content Terms of Use</p>
-                </section>
-              </div>
-              <div className="col">
-                <h5 className="fs-5 fw-bold">Company</h5>
-                <section
-                  //   style={{ fontFamily: "Proxima Nova" }}
-                  className="options"
-                >
-                  <p>About HyperX</p>
-                  <p>About HP</p>
-                  <p>Careers</p>
-                  <p>Press</p>
-                  <p>Privacy</p>
-                  <p>Terms of Use</p>
-                  <p>Limited Warranty Statement</p>
-                  <p>Accessibility</p>
-                  <p>Use of Cookies</p>
-                  <p>Personal Data Rights</p>
-                  <p>Terms of Service</p>
-                  <p>Refund policy</p>
-                </section>
-              </div>
-              <div className="col">
-                <h5 className="fs-5 fw-bold">Other Resource</h5>
-                <section className="options">
-                  <p>NGENUITY Software</p>
-                  <p>Blog</p>
-                  <p>Partner Program</p>
-                  <p>Student Discount</p>
-                </section>
+      <div className="pt-5 pb-4 bg-black text-white text-center">
+        <div className="container w-25">
+          <h4 className="modelSubtitle fs-4">
+            Join the #HyperXFamily on social!
+          </h4>
+          <p className="pb-4 description">
+            Get a sneak peek at our new products and deals when you sign up to
+            join the HyperX Family.
+          </p>
+          <div className="g-3">
+            <Link
+              to="https://www.instagram.com/hyperx/"
+              target={"_blank"}
+              className="px-2"
+            >
+              <InstagramIcon className="social-icon" />
+            </Link>
+            <Link
+              to="https://www.facebook.com/hyperxcommunity"
+              target={"_blank"}
+              className="px-2"
+            >
+              <FacebookIcon />
+            </Link>
+            <Link
+              to="https://twitter.com/hyperx"
+              target={"_blank"}
+              className="px-2"
+            >
+              <TwitterIcon />
+            </Link>
+            <Link
+              to="https://discord.gg/hyperxgaming"
+              target={"_blank"}
+              className="px-2"
+            >
+              <DiscordIcon />
+            </Link>
+            <Link
+              to="https://www.tiktok.com/@hyperx?lang=en"
+              target={"_blank"}
+              className="px-2"
+            >
+              <TikTokIcon />
+            </Link>
+            <Link
+              to="https://www.twitch.tv/hyperx"
+              target={"_blank"}
+              className="px-2"
+            >
+              <TwitchIcon />
+            </Link>
+            <Link
+              to="https://www.youtube.com/c/hyperx"
+              target={"_blank"}
+              className="px-2"
+            >
+              <YouTubeIcon />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="pt-5 footer-background">
+        <div className="container">
+          <div className="row">
+            <div className="col-2">
+              <h5 className="fs-5 fw-bold modelSubtitle">Suport</h5>
+              <section className="description">
+                <p>Product Suport</p>
+                <p>Contact Suport</p>
+                <p>Shipping Information</p>
+                <p>Refund Policy</p>
+                <p>HP/HYPERX Visual Content Terms of Use</p>
+              </section>
+            </div>
+            <div className="col-2">
+              <h5 className="fs-5 fw-bold modelSubtitle">Company</h5>
+              <section
+                //   style={{ fontFamily: "Proxima Nova" }}
+                className="description"
+              >
+                <p>About HyperX</p>
+                <p>About HP</p>
+                <p>Careers</p>
+                <p>Press</p>
+                <p>Privacy</p>
+                <p>Terms of Use</p>
+                <p>Limited Warranty Statement</p>
+                <p>Accessibility</p>
+                <p>Use of Cookies</p>
+                <p>Personal Data Rights</p>
+                <p>Terms of Service</p>
+                <p>Refund policy</p>
+              </section>
+            </div>
+            <div className="col-2">
+              <h5 className="fs-5 fw-bold modelSubtitle">Other Resource</h5>
+              <section className="description">
+                <p>NGENUITY Software</p>
+                <p>Blog</p>
+                <p>Partner Program</p>
+                <p>Student Discount</p>
+              </section>
+            </div>
+            <div className="col-6">
+              <div className="d-flex justify-content-end">
+                <div className="footer-logo">
+                  <img width={210} src={Logo} alt="" />
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="col">
-            <Logo />
           </div>
         </div>
       </div>
