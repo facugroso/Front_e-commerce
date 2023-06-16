487b8c63d8b6eb2032d0919c78ba596cf58bb5b4
facugroso <110434971+facugroso@users.noreply.github.com>
2023-06-16 09:11:07 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,60 @@
+import Logo from "../assets/icons/Logo";
+
 function Footer() {
   return (
     <>
-      <div className="container">
+      <div className="conteiner">
         <div className="row">
           <div className="col">
-            <h1>Suport</h1>
-            <p>Product Suport</p>
-            <p>Contact Suport</p>
-            <p>Shipping Information</p>
-            <p>Refund Policy</p>
-            <p>HP/HYPERX Visual Content Terms of Use</p>
+            <div className="row">
+              <div className="col">
+                <h5 className="fs-5 fw-bold">Suport</h5>
+                <section className="options">
+                  <p>Product Suport</p>
+                  <p>Contact Suport</p>
+                  <p>Shipping Information</p>
+                  <p>Refund Policy</p>
+                  <p>HP/HYPERX Visual Content Terms of Use</p>
+                </section>
+              </div>
+              <div className="col">
+                <h5 className="fs-5 fw-bold">Company</h5>
+                <section
+                  //   style={{ fontFamily: "Proxima Nova" }}
+                  className="options"
+                >
+                  <p>About HyperX</p>
+                  <p>About HP</p>
+                  <p>Careers</p>
+                  <p>Press</p>
+                  <p>Privacy</p>
+                  <p>Terms of Use</p>
+                  <p>Limited Warranty Statement</p>
+                  <p>Accessibility</p>
+                  <p>Use of Cookies</p>
+                  <p>Personal Data Rights</p>
+                  <p>Terms of Service</p>
+                  <p>Refund policy</p>
+                </section>
+              </div>
+              <div className="col">
+                <h5 className="fs-5 fw-bold">Other Resource</h5>
+                <section className="options">
+                  <p>NGENUITY Software</p>
+                  <p>Blog</p>
+                  <p>Partner Program</p>
+                  <p>Student Discount</p>
+                </section>
+              </div>
+            </div>
           </div>
+
           <div className="col">
-            <h1>Company</h1>
-            <p>About HyperX</p>
-            <p>About HP</p>
-            <p>Careers</p>
-            <p>Press</p>
-            <p>Privacy</p>
-            <p>Terms of Use</p>
-            <p>Limited Warranty Statement</p>
-            <p>Accessibility</p>
-            <p>Use of Cookies</p>
-            <p>Personal Data Rights</p>
-            <p>Terms of Service</p>
-            <p>Refund policy</p>
-          </div>
-          <div className="col">
-            <h1>Other Resource</h1>
-            <p>NGENUITY Software</p>
-            <p>Blog</p>
-            <p>Partner Program</p>
-            <p>Student Discount</p>
-          </div>
-          <div className="col">
-            <img src="" alt="IMAGEN HIPERX" />
+            <Logo />
           </div>
         </div>
       </div>
+      ;
     </>
   );
 }
